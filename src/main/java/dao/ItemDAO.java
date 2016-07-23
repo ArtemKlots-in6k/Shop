cd5bae7fb30c46fba46b1ed8af82d694a8064474
Biosss <bios_ss@mail.ru>
2016-07-23 22:55:37 +0300

    Рефакторинг. ItemDAO теперь наследуется от абстрактного класса DAO. Логика перемещена в абстрактній класс

--- src/main/java/dao/ItemDAO.java
+++ src/main/java/dao/ItemDAO.java
@@ -11,8 +11,11 @@ import java.util.List;
 /**
  * Created by Artem Klots on 7/21/16.
  */
-public class ItemDAO {
-    private Statement statement;
+public class ItemDAO extends DAO {
+
+    public ItemDAO() {
+        tableName = "items";
+    }
 
     public Item getItemById(int id) throws SQLException {
         setUpConnection();
@@ -27,21 +30,5 @@ public class ItemDAO {
         int categoryId = result.getInt("category_id");
         BigDecimal price = result.getBigDecimal("price");
         return new Item(id, title, new CategoryDAO().getCategoryById(categoryId), price);
-    }
-
-    public List<Item> getAll() throws SQLException {
-        List<Item> items = new ArrayList<Item>();
-        setUpConnection();
-
-        ResultSet result = statement.executeQuery("SELECT * FROM items ");
-        while (result.next()) {
-            items.add(parse(result));
-        }
-        return items;
-    }
-
-    private void setUpConnection() throws SQLException {
-        Connection connection = new ConnectionFactoryImpl().getConnection();
-        statement = connection.createStatement();
     }
 }
