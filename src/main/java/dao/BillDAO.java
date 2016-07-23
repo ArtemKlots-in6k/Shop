e08f7f6e35407dc7fa153f87cbcc9837a7b14552
Biosss <bios_ss@mail.ru>
2016-07-23 20:09:37 +0300

    Выполнено задание: для конкретного пользователя вернуть список всех заказов

--- src/main/java/dao/BillDAO.java
+++ src/main/java/dao/BillDAO.java
@@ -2,11 +2,10 @@ package dao;
 
 import ConnectionFactory.ConnectionFactoryImpl;
 import entity.Bill;
-import entity.User;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Date;
+import java.util.*;
 
 /**
  * Created by Artem Klots on 7/22/16.
@@ -19,6 +18,23 @@ public class BillDAO {
         ResultSet result = statement.executeQuery("SELECT * FROM bills WHERE id = " + id);
         result.next();
         return parse(result);
+    }
+
+    public Map<Integer, Map<Integer, Date>> getAllBillsByUserId(int id) throws SQLException {
+        Map<Integer, Map<Integer, Date>> result = new LinkedHashMap<>();
+        setUpConnection();
+
+        ResultSet response = statement.executeQuery("SELECT bills.id, date, SUM(purchases.price) AS amount " +
+                "FROM bills " +
+                "LEFT JOIN purchases on bill_id = bills.id " +
+                "WHERE user_id = '" + id + "' " +
+                "GROUP BY bills.id ");
+        while (response.next()) {
+            Map<Integer, Date> bill = new LinkedHashMap<>();
+            bill.put(response.getInt("amount"), response.getDate("date"));
+            result.put(response.getInt("id"), bill);
+        }
+        return result;
     }
 
     public List<Bill> getAll() throws SQLException {
--- src/test/java/BillDAOTest.java
+++ src/test/java/BillDAOTest.java
@@ -5,7 +5,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -14,10 +16,12 @@ import static org.junit.Assert.assertThat;
  * Created by Artem Klots on 7/22/16.
  */
 public class BillDAOTest extends DatabaseInitializer {
+    BillDAO billDAO;
     @Before
     public void setUp() throws Exception {
         prepareConnection();
         super.setUp();
+        billDAO = new BillDAO();
     }
 
     @After
@@ -28,15 +32,18 @@ public class BillDAOTest extends DatabaseInitializer {
 
     @Test
     public void getBillById() throws Exception {
-        BillDAO billDAO = new BillDAO();
-
         Bill expectedBill = billDAO.getBillById(0);
         assertThat(expectedBill.getId(), is(0));
     }
     @Test
     public void getAll() throws Exception {
-        BillDAO billDAO = new BillDAO();
-
         assertThat(billDAO.getAll().size(), is(3));
+    }
+
+    @Test
+    public void getAllBillsByUserId() throws Exception {
+        int userId = 1;
+        Map result = billDAO.getAllBillsByUserId(userId);
+        assertThat(result.get(0).toString(), is("{2800=2016-07-18}"));
     }
 }
